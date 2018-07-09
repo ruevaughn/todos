201931c7751ad07e25df1d632a0ab90c89ba68cc
Chase Jensen <soccerchase@gmail.com>
2018-07-08 19:50:57 -0600

    Add User to Database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_173953) do
+ActiveRecord::Schema.define(version: 2018_07_09_014940) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
